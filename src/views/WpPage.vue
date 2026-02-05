b4e84369da4df47ac044005d36190bc9a80908d8
Mike Beasley <mbeasley4@gmail.com>
2026-02-05 12:10:38 -0500

    update to remove loading

--- src/views/WpPage.vue
+++ src/views/WpPage.vue
@@ -7,13 +7,11 @@ import WpBlockRenderer from '@/components/WpBlockRenderer.vue'
 
 const route = useRoute()
 const page = ref<any>(null)
-const loading = ref(true)
 
-async function loadPage() {
-  loading.value = true
+async function loadPage() {  
   const { data } = await wp.get(`/wp/v2/pages?slug=${route.params.slug}`)
   page.value = data?.[0] ?? null
-  loading.value = false
+
 }
 
 onMounted(loadPage)
@@ -21,7 +19,6 @@ watch(() => route.params.slug, loadPage)
 </script>
 
 <template>
-  <div v-if="loading" class="p-12">Loading…</div>
-  <NotFound v-else-if="!page" />
+  <NotFound v-if="!page" />
   <WpBlockRenderer :page="page" />
 </template>
